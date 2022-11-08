067faab285d1d05aef02c53bb2f5fd748f179742
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2022-11-08 17:49:37 -0300

    Removendo comentários e console.log

--- pages/index.js
+++ pages/index.js
@@ -63,34 +63,33 @@ function Header() {
 }
 
 function Timeline(propriedades) {
-    // console.log("Dentro do componente", propriedades.playlists);
     const playlistNames = Object.keys(propriedades.playlists);
+
     // Statement
     // Retorno por expressão
+
     return (
         <StyledTimeline>
             { playlistNames.map((playlistName) => {
-                const videos = propriedades.playlists[playlistName];
-                console.log(playlistName);
-                console.log(videos);
-                return (
-                    <section>
-                        <h2>{ playlistName }</h2>
-                        <div>
-                            { videos.map((video) => {
-                                return (
-                                    <a href={ video.url }>
-                                        <img src={ video.thumb }/>
-                                        <span>
-                                            { video.title }
-                                        </span>
-                                    </a>
-                                )
-                            }) }
-                        </div>
-                    </section>
-                )
-            }) }
+                    const videos = propriedades.playlists[playlistName];
+                    return (
+                        <section>
+                            <h2>{ playlistName }</h2>
+                            <div>
+                                { videos.map((video) => {
+                                        return (
+                                            <a href={ video.url }>
+                                                <img src={ video.thumb }/>
+                                                <span> { video.title } </span>
+                                            </a>
+                                        )
+                                    }
+                                ) }
+                            </div>
+                        </section>
+                    )
+                }
+            ) }
         </StyledTimeline>
     )
 }
